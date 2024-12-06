c4287e1f01c605a2a7f347848c1e82675948c7e7
yinkus89 <yinkawlb@gmail.com>
2024-12-06 20:55:36 +0100

    done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-// server.ts
-import { app, port } from './app';  // Import app and port from app.ts
+// src/app.tsx
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+import express from 'express';
+
+// Initialize the app
+const app = express();
+
+// Define the port
+const port = 5000;
+
+export { app, port };
